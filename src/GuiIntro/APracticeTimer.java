434ba9cea138bdceabcb93abd1b06bc7a7deb943
Austin Cheng <austinbicheng@gmail.com>
2017-11-01 18:11:48 -0700

    one timer issue, one hour gone

---
+++ src/GuiIntro/APracticeTimer.java
@@ -0,0 +1,24 @@
+package GuiIntro;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class APracticeTimer
+{
+	public static void main(String args[])
+	{
+		//new JFrame().setVisible(true);
+		ActionListener actionListener = new ActionListener()
+		{
+			public void actionPerformed(ActionEvent actionEvent)
+			{
+				System.out.println("Hello World Timer");
+			}
+		};
+		Timer timer = new Timer(500, actionListener);
+		timer.start();
+	}
+}
